fc8512031034ce0bdb23cc6b225f7a2fd02cbbc9
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-08-13 16:11:29 +0530

    fix Homebrew formula checks (#34)

--- Formula/appwrite.rb
+++ Formula/appwrite.rb
@@ -1,7 +1,6 @@
 class Appwrite < Formula
   desc "Command-line tool for interacting with the Appwrite API"
   homepage "https://appwrite.io"
-  version "26.1.0"
   license "BSD-3-Clause"
 
   def self.binary_arch
@@ -33,20 +32,20 @@ class Appwrite < Formula
   # Release automation injects per-target SHA256 values when publishing binaries.
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/appwrite/sdk-for-cli/releases/download/#{version}/appwrite-cli-darwin-arm64"
+      url "https://github.com/appwrite/sdk-for-cli/releases/download/26.1.0/appwrite-cli-darwin-arm64"
       sha256 "ded0d0d2fee52b1f9dfa7a3960d716e5114682c8f2867f0a7f9a2ebb8d7b6cd8"
     else
-      url "https://github.com/appwrite/sdk-for-cli/releases/download/#{version}/appwrite-cli-darwin-x64"
+      url "https://github.com/appwrite/sdk-for-cli/releases/download/26.1.0/appwrite-cli-darwin-x64"
       sha256 "bf5119e689bbb4fce7c708c9fd3d38de4c052d656dca6148599462538c2cc45e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/appwrite/sdk-for-cli/releases/download/#{version}/appwrite-cli-linux-arm64"
+      url "https://github.com/appwrite/sdk-for-cli/releases/download/26.1.0/appwrite-cli-linux-arm64"
       sha256 "f846d15ec74164eb2d25777583e661a40f7d2805e81e7f2cbdf5f7433a173bd3"
     else
-      url "https://github.com/appwrite/sdk-for-cli/releases/download/#{version}/appwrite-cli-linux-x64"
+      url "https://github.com/appwrite/sdk-for-cli/releases/download/26.1.0/appwrite-cli-linux-x64"
       sha256 "292b47bbb62d57a0c35c4fde1d9f100a72d8a3caafdb61448aa635ed04d1a0ae"
     end
   end
@@ -66,7 +65,7 @@ class Appwrite < Formula
   end
 
   test do
-    assert_match "Usage:", shell_output("#{bin}/appwrite --help")
+    assert_match "USAGE", shell_output("#{bin}/appwrite --help")
     assert_match "compdef", shell_output("#{bin}/appwrite completion zsh")
   end
 end
